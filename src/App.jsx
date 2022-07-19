ff96574283af827eb1fb8fad150f98cd5c17cb8f
Mateusz <matesz1kb@gmail.com>
2022-07-19 23:51:39 +0200

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
-import styledEngine from '@mui/styled-engine';
-import { themeOptions } from './styles/theme';
+import styled from 'styled-components';
+import { theme } from './styles/theme';
+import { ThemeProvider } from '@mui/system';
+import { CssBaseline } from '@mui/material';
+import { routes } from './routes/routes';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
-const AppContainer = styledEngine.main`
-  width: 100%;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const AppContainer = styled.main`
+   width: 100%;
+   min-height: 100vh;
+   display: flex;
+   justify-content: center;
+   align-items: center;
 `;
+
+let routesHandler = (
+   <Switch>
+      {routes.map((route) => (
+         <Route path={route.path} exact key={route.path}>
+            {route.component}
+         </Route>
+      ))}
+   </Switch>
+);
 
 function App() {
    return (
-      <ThemeProvider theme={themeOptions}>
-         <AppContainer>Keyboardify</AppContainer>
+      <ThemeProvider theme={theme}>
+         <CssBaseline />
+         <BrowserRouter>
+            {routesHandler}
+            <ul>
+               {routes.map((route, index) => (
+                  <li key={index}>
+                     <Link to={route.path}>{route.name}</Link>
+                  </li>
+               ))}
+            </ul>
+         </BrowserRouter>
       </ThemeProvider>
    );
 }
---
+++ src/routes/routes.jsx
@@ -0,0 +1,9 @@
+import Product from '../pages/Product';
+
+export const routes = [
+   {
+      path: '/product',
+      name: 'Product',
+      component: <Product />
+   }
+];
