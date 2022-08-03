a488a5e0488556301bccb65b012230e6dd3376d7
Mateusz <matesz1kb@gmail.com>
2022-08-03 21:36:13 +0200

    Added edge cases to format price func

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -16,6 +16,11 @@ export const ProductCard = (props) => {
 
    const formatPrice = (value) => {
       const stringPrice = value.toString();
+      if (stringPrice.length === 1) {
+         return `0.0${stringPrice}`;
+      } else if (stringPrice.length === 2) {
+         return `0.${stringPrice.substring(stringPrice.length - 2, stringPrice.length)}`;
+      }
       return `${stringPrice.substring(0, stringPrice.length - 2)}.${stringPrice.substring(
          stringPrice.length - 2,
          stringPrice.length
