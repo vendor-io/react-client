69eb7df609d617b2409bc204155f351ccce1aec0
Mateusz <matesz1kb@gmail.com>
2022-08-03 23:21:42 +0200

    Adjusted Fab styling

--- src/components/CartFab.jsx
+++ src/components/CartFab.jsx
@@ -6,12 +6,12 @@ export const CartFab = () => {
    return (
       <Box
          sx={{
-            position: 'absolute',
+            position: 'fixed',
             zIndex: 9999,
             bottom: '30px',
             right: '25px'
          }}>
-         <Tooltip title="View cart" arrow placement="left-start">
+         <Tooltip title="View cart" arrow placement="top-start">
             <Fab
                component={RouterLink}
                color="primary"
