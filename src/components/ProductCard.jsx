da1f0617a9700fda2f436d08c4ed8a21ae40b9fa
Mateusz <matesz1kb@gmail.com>
2022-08-04 02:15:25 +0200

    Adjusted ProductCard styling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import {
+   Box,
    Grid,
    Card,
    CardActions,
@@ -26,23 +27,33 @@ export const ProductCard = (props) => {
          stringPrice.length
       )}`;
    };
+
    return (
-      <Card>
-         <CardMedia component="img" alt={name} height="140" image={image} />
-         <CardContent>
-            <Link
-               gutterBottom
-               component={RouterLink}
-               sx={{ display: 'block' }}
-               to={`/products/${id}`}
-               variant="h5">
-               {name}
-            </Link>
-            <Typography variant="button" color="text.secondary" textAlign="right">
-               {formatPrice(price)} $
-            </Typography>
-         </CardContent>
-         <CardActions>
+      <Card
+         sx={{
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            flexGrow: 1,
+            justifyContent: 'space-between'
+         }}>
+         <Box>
+            <CardMedia component="img" alt={name} height="140" image={image} />
+            <CardContent>
+               <Link
+                  gutterBottom
+                  component={RouterLink}
+                  sx={{ display: 'block' }}
+                  to={`/products/${id}`}
+                  variant="h5">
+                  {name}
+               </Link>
+               <Typography variant="button" color="text.secondary" textAlign="right">
+                  {formatPrice(price)} $
+               </Typography>
+            </CardContent>
+         </Box>
+         <CardActions sx={{ pb: 2, pr: 1 }}>
             <Grid container sx={{ justifyContent: 'flex-end', alignItems: 'center' }}>
                <Grid item>
                   <Button startIcon={<ShoppingCartIcon />} variant="contained">
