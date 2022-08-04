2303e8cf4acf33f6b015ba3aa73b0a523c50e5d2
Mateusz <matesz1kb@gmail.com>
2022-08-04 02:03:20 +0200

    Added slug to breadcrumbs

--- src/pages/AllCategoryProducts.jsx
+++ src/pages/AllCategoryProducts.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { useProduct } from './../hooks/useProduct';
 import { useAuth } from './../hooks/useAuth';
+import { BreadcrumbsContext } from './../context/breadcrumbs-context';
 
 import { ProductList } from './../components/ProductList';
 import { ProductListSkeleton } from './../components/ProductListSkeleton';
@@ -10,6 +11,8 @@ function AllCategoryProducts() {
    const [products, setProducts] = useState([]);
    const [isLoading, setIsLoading] = useState(true);
 
+   const { setCurrentBreadcrumb } = useContext(BreadcrumbsContext);
+
    const { slug } = useParams();
    const { token } = useAuth();
    const { getProductsFromCategory } = useProduct();
@@ -17,8 +20,15 @@ function AllCategoryProducts() {
    useEffect(() => {
       if (token) {
          getProductsFromCategory(token, slug).then((data) => setProducts(data));
+
+         let formattedSlug = slug;
+         formattedSlug = formattedSlug.charAt(0).toUpperCase() + formattedSlug.slice(1);
+         formattedSlug = formattedSlug.replace('-', ' ');
+         setCurrentBreadcrumb(formattedSlug);
+
          setIsLoading(false);
       }
+      return () => setCurrentBreadcrumb(null);
    }, [token, slug]);
 
    if (isLoading) {
