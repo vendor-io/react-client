26ed43bba9883186420f214e84d3dc80865ec9b8
Mateusz <matesz1kb@gmail.com>
2022-08-06 03:29:29 +0200

    Todo Homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
+import { Container, Paper, Typography } from '@mui/material';
+
 function Home() {
-   return <div>Home</div>;
+   //TODO: Homepage
+   return (
+      <Container component="main" maxWidth="xl" sx={{ mb: 4 }}>
+         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+            <Typography component="h1" variant="h4" align="center">
+               Home {'(todo)'}
+            </Typography>
+         </Paper>
+      </Container>
+   );
 }
 
 export default Home;
