15d4410a11b83aac36546e14fe33fcf589e3161d
Mateusz <matesz1kb@gmail.com>
2022-08-08 00:26:28 +0200

    Added useOrder hook to handle orders

---
+++ src/hooks/useOrder.jsx
@@ -0,0 +1,35 @@
+export function useOrder() {
+   const getOrdersForUser = async (token, userId) => {
+      let orders;
+      await fetch(`${import.meta.env.VITE_BACKEND_SERVER}/api/orders/${userId}`, {
+         method: 'GET',
+         mode: 'cors',
+         headers: {
+            Authorization: `Bearer ${token}`
+         }
+      })
+         .then((res) => res.json())
+         .then((data) => (orders = data));
+
+      return orders;
+   };
+
+   const createOrder = async (token, requestBody) => {
+      let order;
+      await fetch(`${import.meta.env.VITE_BACKEND_SERVER}/api/orders/new`, {
+         method: 'POST',
+         mode: 'cors',
+         headers: {
+            Authorization: `Bearer ${token}`,
+            'content-type': 'application/json;charset=UTF-8'
+         },
+         body: JSON.stringify(requestBody)
+      })
+         .then((res) => res.json())
+         .then((data) => (order = data));
+
+      return order;
+   };
+
+   return { getOrdersForUser, createOrder };
+}
